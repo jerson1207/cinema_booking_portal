44037ff535e683d8dec26d8c6ab6476ee805205c
jerson1207 <jersonjabatstudent@gmail.com>
2022-02-06 11:56:37 +0800

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+admin = User.new(first_name: "Jerson", last_name: "Jabat", mobile_number: "09123456789", email: "admin@test.com", password: "123456", password_confirmation: "123456", role: 1)
+admin.save
+
+user = User.new(first_name: "James", last_name: "Bond", mobile_number: "09123456789", email: "user1@test.com", password: "123456", password_confirmation: "123456", role: 0)
+user.save
