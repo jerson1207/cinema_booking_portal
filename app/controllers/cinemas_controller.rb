1a9aa2ab7912b3cb7b637e68aa7f97b1bc47eb4a
jerson1207 <jersonjabatstudent@gmail.com>
2022-02-06 14:37:00 +0800

    adding design

--- app/controllers/cinemas_controller.rb
+++ app/controllers/cinemas_controller.rb
@@ -2,4 +2,8 @@ class CinemasController < ApplicationController
   def index
     @cinemas = Cinema.all
   end
+
+  def show
+    @cinema = Cinema.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  root 'cinemas#index'
+  root 'static_pages#home'
   get 'sessions/new'
   resources :users, except: [:index, :show, :edit] 
   get 'sessions/new'
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
 
-  resources :cinemas, only: :index do
+  resources :cinemas, only: [:index, :show] do
     resources :seats do
       resources :time_slots do
         resources :ticket
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
 
   get '/book_movies', to: 'static_pages#book_movies'
   get '/about', to: 'static_pages#about'
+  get '/home', to: 'static_pages#home'
 
 
   namespace :admin do
